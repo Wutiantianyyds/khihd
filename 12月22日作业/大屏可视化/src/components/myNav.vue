0b4accabfa69263f50e9c9ece4b748f9688305fd
Wutiantianyyds <2141152825@qq.com>
2022-12-22 23:36:26 +0800

    作业

---
+++ 12月22日作业/大屏可视化/src/components/myNav.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="nav">导航</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+  created() {},
+  mounted() {},
+  components: {},
+  computed: {},
+  watch: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.nav {
+  width: 95%;
+  height: 25%;
+  background-color: pink;
+  padding: 20px;
+  margin: 20px 20px;
+}
+</style>
